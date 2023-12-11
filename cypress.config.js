739ce93d9ce82078d67141848ea7bd3f6b97807b
Quynh Nguyen <quynhnguyen.swe@gmail.com>
2023-12-11 15:13:04 +0100

    Testing Open New Tab

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  env: {
+    url: 'https://qaplayground.dev/#apps',
+  },
+  e2e: {
+    specPattern: 'cypress/testingFolders/*.js',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    
+    },
+
+  }
+  },
+);
