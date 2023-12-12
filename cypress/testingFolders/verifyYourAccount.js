ebcd14b148ab88ad9515cc79bfc1a2715c43159d
Quynh Nguyen <quynhnguyen.swe@gmail.com>
2023-12-12 11:16:56 +0100

    Testing Verify Your Account

---
+++ cypress/testingFolders/verifyYourAccount.js
@@ -0,0 +1,22 @@
+/// <reference types="Cypress" />
+//describe part "My First Test" is a test suite.
+
+describe('Playing around mini apps', function () {
+    it('testing Verify Your Account', function () {
+
+        //Enter valid code by pressing key-up button or typing number and assert success message
+
+        cy.visit(Cypress.env('url'))
+        cy.contains('Verify Your Account').click()
+
+        //1st way: type number
+        for (let i = 1; i <= 6; i++) {
+            // const selector = `div input:nth-child(${i})`; 
+            cy.get(`div input:nth-child(${i})`).type('9');
+            }
+        cy.get('div.container').should('include.text','Success')
+
+        })
+    })
+
+    //Lession learned: The ${i} syntax is called a template literal, and it's a way to embed variables or expressions inside a string in JavaScript.
