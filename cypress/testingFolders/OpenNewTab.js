dc418f1f22aafb6d3754c97593de64259ef525a1
Quynh Nguyen <quynhnguyen.swe@gmail.com>
2023-12-18 19:58:46 +0100

    resolve conflicts

--- cypress/testingFolders/OpenNewTab.js
+++ cypress/testingFolders/OpenNewTab.js
@@ -1,10 +1,8 @@
 /// <reference types="Cypress" />
 //describe part "My First Test" is a test suite.
 
-describe("Playing around mini apps", function () {
-  it("OpenNewTab", function () {
-    //Open new tab by clicking on the button
-    //and assert the text on the new opened page
+describe('Playing around mini apps', function () {
+    it('testing Open New Tab', function () {
 
     cy.visit(Cypress.env("url"));
     cy.get(".card-content").contains("New Tab").click();
@@ -12,7 +10,15 @@ describe("Playing around mini apps", function () {
     cy.get(".btn.btn-lg").invoke("removeAttr", "target").click();
     cy.get("#wrapper").should("include.text", "Welcome to the new page!");
 
-    // Lesson learned: if the new opened page (child) has the same url with its
-    //parents. Should not use cy.origin()
-  });
-});
+        cy.visit(Cypress.env('url'))
+        cy.get('.card-content').contains('New Tab').click()
+        //Use the invoke command to perform an action on element with class .btn-btn-lg
+        cy.get('.btn.btn-lg').invoke('removeAttr','target').click()
+        cy.get('#wrapper').should('include.text','Welcome to the new page!')
+        
+
+        
+    })
+})
+
+//Lession learned: if the new opened page (child) has the same url with its parents. Should not use cy.origin()
--- cypress/testingFolders/dynamicTable.js
+++ cypress/testingFolders/dynamicTable.js
@@ -1,9 +1,8 @@
 /// <reference types="Cypress" />
 //describe part "My First Test" is a test suite.
 
-describe("Playing around mini apps", function () {
-  it("Dynamic Table", function () {
-    // Find the Spider-Man in a table that changes the order of rows and assert his real name
+describe('Playing around mini apps', function () {
+    it('testing Dynamic Table', function () {
 
     cy.visit(Cypress.env("url"));
     cy.contains("Dynamic Table").click();
@@ -16,7 +15,18 @@ describe("Playing around mini apps", function () {
         );
       }
 
-      // Lesson learned: when selecting elements in dynamic table, make sure to select all table rows (not table itself)
-    });
-  });
-});
+        cy.visit(Cypress.env('url'))
+        cy.contains('Dynamic Table').click()
+        cy.get('tbody tr').each(($e1, index, $list) => {
+            const superHero = $e1.text()
+            if (superHero.includes('Spider-Man')) {
+                cy.get('tbody tr td span.text-sm').should('include.text', 'Peter Parker')
+            }
+        
+            
+        })
+    })
+
+})
+
+//Lession learned: when selecting elements in dynamic table, make sure to select all table rows (not table itself)
